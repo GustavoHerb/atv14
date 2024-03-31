6185c6454620aedc3b36d0cbd8b41a76d93c3956
GustavoHerb <141774477+GustavoHerb@users.noreply.github.com>
2024-03-31 16:09:30 -0300

    Add files via upload

---
+++ script7.js
@@ -0,0 +1,5 @@
+const usuarios = [
+    {"id": 1, "email": "email@email.com", "senha": "senha123"},
+    {"id": 2, "email": "infinity@email.com", "senha": "infinity123"},
+    {"id": 3, "email": "exemplo@email.com", "senha": "exemplo123"}
+];
